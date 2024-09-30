a314063b27b6a0cbe05ea04ea22fa229a9f8769a
GuitarJack1 <tigerjack07@icloud.com>
2024-09-30 07:10:49 -0500

    Started adding planet data

--- Assets/Scripts/ExplorationPlanet.cs
+++ Assets/Scripts/ExplorationPlanet.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class ExplorationPlanet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string planetName;
+    public float mass;
+    public float size;
 
-    // Update is called once per frame
-    void Update()
+    public Color color;
+    public float emissionIntensity;
+
+    public float startXPos;
+    public float startZPos;
+
+    public float startXVel;
+    public float startZVel;
+
+    public ExplorationPlanet(string planetName, float mass, float size, Color color, float emissionIntensity, float startXPos, float startZPos, float startXVel, float startZVel)
     {
-        
+        this.planetName = planetName;
+        this.mass = mass;
+        this.size = size;
+        this.color = color;
+        this.emissionIntensity = emissionIntensity;
+        this.startXPos = startXPos;
+        this.startZPos = startZPos;
+        this.startXVel = startXVel;
+        this.startZVel = startZVel;
     }
 }
